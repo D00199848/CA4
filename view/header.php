eca185a5c4651344401f052ef9aec10d19bd247b
D00213436 <47351845+D00213436@users.noreply.github.com>
2019-04-02 15:12:32 +0100

    Testing connection with GitHub

---
+++ view/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    
+
+    
+    <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="#band">BANDS</a></li>
+        <li><a href="#tour">GIGS & TICKETS</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+        </ul>
+   </div> 
+    </nav>
+
+            <a href="login.php">Log In</a>
+    <title>Tickets Direct</title>
+    <link href="./main.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Tickets Direct</h1></header>
+       
